75513a3bac168d39ea0f76bcaf786f2d6d72f075
Lucas Castro <lcs147@hotmail.com>
2025-10-03 16:00:37 -0400

    wrong answer but compiling

--- main.cpp
+++ main.cpp
@@ -5,15 +5,16 @@ using namespace std;
 #define fastio ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
  
 #ifdef LOCAL
-#include "../cp-lib/helpers/debug.cpp"
+#include "cp-lib/helpers/debug.cpp"
 #else
 #define debug(...)
 #endif
 
 template<typename wT>
 struct newspp {
-    int n;
-    const int oo = numeric_limits<wT>::max{} / 10;
+    int n, k, t;
+    // const wT oo = numeric_limits<wT>::max() / 10;
+    const wT oo = 1e9 / 10;
 
     vector<vector<pair<int, wT>>> ori_adj;
     vector<vector<pair<int, wT>>> adj;
@@ -23,22 +24,10 @@ struct newspp {
     newspp(int n_): n(n_) {
         ori_adj.assign(n, {});
         neig.assign(n, {});
+        k = floor(pow(log2(n), 1.0 / 3.0));
+        t = floor(pow(log2(n), 2.0 / 3.0));
     }
-
-    int toAnyCustomNode(int real_id) {
-        return neig[real_id].begin()->second;
-    }
-    vector<wT> execute(int s) {
-        s = toAnyCustomNode(s);
-        fill(d.begin(), d.end(), oo);
-        bmssp(l, oo, s);
-
-        vector<wT> res(n);
-        for(int i = 0; i < n; i++) res[i] = d[toAnyCustomNode(i)];
-        return res;
-    }
-
-    void add(int a, int b, wT w) {
+    void addEdge(int a, int b, wT w) {
         ori_adj[a].emplace_back(b, w);
     }
 
@@ -59,11 +48,11 @@ struct newspp {
         dad.assign(cnt, {});
         root.assign(cnt, {});
 
-        for(int i = 0; i < n; i++) { // create 0 weight cycles
+        for(int i = 0; i < n; i++) { // create 0-weight cycles
             for(auto cur = neig[i].begin(); cur != neig[i].end(); cur++) {
                 auto nxt = next(cur);
                 if(nxt == neig[i].end()) nxt = neig[i].begin();
-                adj[cur->second].emplace_back(nxt->second);
+                adj[cur->second].emplace_back(nxt->second, wT());
             }
         }
         for(int i = 0; i < n; i++) { // add edges
@@ -75,9 +64,93 @@ struct newspp {
             }
         }
     }
+    
+    int toAnyCustomNode(int real_id) {
+        return neig[real_id].begin()->second;
+    }
+
+    vector<wT> execute(int s) {
+        s = toAnyCustomNode(s);
+        fill(d.begin(), d.end(), oo);
+        d[s] = 0;
+        
+        const int l = ceil(log2(n) / t);
+        bmssp(l, oo, {s});
+
+        vector<wT> res(n);
+        for(int i = 0; i < n; i++) res[i] = d[toAnyCustomNode(i)];
+        return res;
+    }
+    
+    // ===================================================================
+    
+    struct batchPQ {
+        int n;
+        using elementT = pair<wT, int>;
+        set<elementT> s;
+        map<int, wT> inv;
+        int M, B;
+
+        batchPQ(int M_, int B_): M(M_), B(B_) {}
+        unsigned size() {
+            return s.size();
+        }
+        void insert(elementT x) {
+            s.insert(x);
+            inv[x.second] = x.first;
+        }
+        void batchPrepend(const vector<elementT> &v) {
+            for(auto &x: v) {
+                auto it = inv.find(x.second);
+                if(it != s.end() && it->second > x.first) {
+                    s.erase({it->second, it->first});
+                    inv.erase(it);
+                }
+                s.insert(x);
+                inv[x.second] = x.first;
+            }
+        }
+        pair<wT, vector<int>> pull() {
+            wT bigKey;
+            vector<int> res;
+            while(s.size() && res.size() < M) {
+                res.push_back(s.begin()->second);
+                bigKey = s.begin()->first;
+                s.erase(s.begin());
+            }
+            int x = B;
+            if(s.size()) {
+                x = s.begin()->first;
+                assert(x > bigKey);
+            }
+            return {x, res};
+        }
+    };
+
+    // set stuff
+    template<typename T>
+    void append(vector<T> &a, auto &b) {
+        a.insert(a.end(), b.begin(), b.end());
+    }
+    template<typename T>
+    void removeDuplicates(vector<T> &v) {
+        unordered_set<T> s(v.begin(), v.end());
+        v.clear();
+        append(v, s);
+    }
+    template<typename T>
+    bool isUnique(vector<T> v) {
+        auto v2 = v;
+        sort(v.begin(), v.end());
+        v.erase(unique(v.begin(), v.end()), v.end());
+        return v2.size() == v.size();
+    }
+
+    // ===================================================================
+
     vector<int> dad, root;
     pair<vector<int>, vector<int>> findPivots(wT B, vector<int> S) {
-        vector<int> w = S;
+        unordered_set<int> w(S.begin(), S.end());
         vector<int> active = S;
         for(int x: S) root[x] = x;
         for(int i = 1; i <= k; i++) {
@@ -93,112 +166,104 @@ struct newspp {
                     }
                 }
             }
-            append(w, nw_active);
+            w.insert(nw_active.begin(), nw_active.end());
             if(w.size() > k * S.size()) {
-                return {S, W};
+                return {S, vector<int>(w.begin(), w.end())};
             }
             swap(active, nw_active);
         }
         vector<int> P;
         for(int x: active) P.push_back(root[x]);
-        return {P, w};
+        removeDuplicates(P);
+
+        return {P, vector<int>(w.begin(), w.end())};
     }
+
     pair<wT, vector<int>> baseCase(wT B, int x) {
         vector<int> found = {x};
 
-        set<wT> h;
-        h.insert({d[x], x});
-        while(h.size()) {
-            auto [u, du] = *h.begin();
-            found.insert(u);
+        set<pair<wT, int>> heap;
+        heap.insert({d[x], x});
+        while(heap.size() && found.size() < k + 1) {
+            auto [du, u] = *heap.begin();
+            found.push_back(u);
             for(auto [v, w]: adj[u]) {
-                if(du + w <= d[v] && du + w <= B) {
-                    s.erase({d[v], v});
+                if(du + w <= d[v] && du + w < B) {
+                    heap.erase({d[v], v});
                     d[v] = du + w;
-                    s.insert({d[v], v});
+                    heap.insert({d[v], v});
                 }
             }
         }
         if(found.size() <= k) return {B, found};
 
         int nB = -1;
-        for(int u: found) nB = max(nb, d[u]);
+        for(int u: found) nB = max(nB, d[u]);
         vector<int> U;
-        for(int u: found) if(d[u] != nB) U.insert(u);
+        for(int u: found) if(d[u] < nB) U.push_back(u);
         return {B, U};
     }
 
-    struct batchPQ {
-
-    };
-
-    template<typename T>
-    void append(vector<T> &a, auto &b) {
-        insert(a.end(), b.begin(), b.end());
-    }
-    template<typename T>
-    void removeDuplicates(vector<T> &v) {
-        unordered_set<T> s(v.begin(), v.end());
-        v.clear();
-        append(v, s);
-    }
-    template<typename T>
-    bool isUnique(const vector<T> &vec) {
-        unordered_set<T> unique_set(vec.begin(), vec.end());
-        return vec.size() == unique_set.size();
-    }
     pair<wT, vector<int>> bmssp(int l, wT B, vector<int> S) {
-        if(l == 0) return baseCase(B, S);
+        assert(S.size() <= (2ll << (l * t)));
+        debug(l, B, S);
+        if(l == 0) return baseCase(B, S[0]);
+
         auto [P, W] = findPivots(B, S);
 
-        const int M = (2ll << ((long long)(l) - 1) * t);
+        const int M = (2ll << ((l - 1) * t));
         batchPQ D(M, B);
-        for(int p: P) D.insert({p, d[p]});
+        for(int p: P) D.insert({d[p], p});
 
         int nB_ = oo;
-        for(int p: P) nB_ = min(nb_, d[p]);
+        for(int p: P) nB_ = min(nB_, d[p]);
 
         vector<int> found; // found
-        while(found.size() < k * (2ll << ((long long)l * t))) {
+        while(found.size() < k * (2ll << ((long long)l * t)) && D.size()) {
             auto [nB, S] = D.pull();
             auto ret = bmssp(l - 1, B, S);
             nB_ = ret.first;
             vector<int> cur_found = ret.second;
             append(found, cur_found);
             
+            vector<pair<wT, int>> promissedButNotFound;
             for(int u: cur_found) {
                 for(auto [v, w]: adj[u]) {
                     if(d[u] + w <= d[v]) {
                         d[v] = d[u] + w;
                         if(nB <= d[v] && d[v] < B) {
-                            D.insert({v, d[v]});
+                            D.insert({d[v], v});
+                        } else if( nB_ <= d[v] && d[v] < nB) {
+                            promissedButNotFound.emplace_back(d[v], v);
                         }
                     }
                 }
             }
-            vector<int> promissedButNotFound;
             for(int x: S) {
-                if(nB_ <= d[x] && d[x] < nB) promissedButNotFound.push_back({x, d[x]});
+                if(nB_ <= d[x] && d[x] < nB) promissedButNotFound.emplace_back(d[x], x);
             }
+            // removeDuplicates(promissedButNotFound);
+            assert(isUnique(promissedButNotFound));
             D.batchPrepend(promissedButNotFound);
         }
-        wT retB = min(nB_, b);
+        wT retB = min(nB_, B);
         
         for(int x: W) if(d[x] < retB) found.push_back(x);
         removeDuplicates(found);
-        return {min(nB_, B), found};
+        return {retB, found};
     }
-}
+};
 
 void solve() {
     int n, m; cin >> n >> m;
     newspp<int> spp(n);
     while(m--) {
         int a, b, w; cin >> a >> b >> w;
-        spp.add(a, b, w);
+        a--; b--;
+        spp.addEdge(a, b, w);
     }
     spp.prepare_graph();
-    auto d = spp.execute();
+    auto d = spp.execute(0);
     for(auto x: d) cout << x << " ";
     cout << endl;
 }
@@ -208,7 +273,7 @@ signed main() {
  
     int t = 1;
     // in(t);
-    L(i, 0, t - 1) solve();
+    for(int i = 0; i < t; i++) solve();
     
     return 0;
 }
