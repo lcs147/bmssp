15ac572a4cf95b39ddd067e08b0931866644fae9
Lucas Castro <lcs147@hotmail.com>
2025-11-26 15:55:58 -0400

    tests for small random graphs added

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -8,8 +8,6 @@
 #include <catch2/catch_test_macros.hpp>
 
 TEST_CASE("King graphs with a wall") {
-
-    // cout << "Local directory: " << std::filesystem::current_path().string() << endl;
     using distT = double;
     vector<string> paths = {"../tests/graphs/grid5.gr", "../tests/graphs/grid25.gr", "../tests/graphs/grid100.gr"};
     for(string path: paths) {
@@ -23,6 +21,25 @@ TEST_CASE("King graphs with a wall") {
         int grid_sz = sqrt(n);
         for(int source: {0, grid_sz - 1, grid_sz * (grid_sz - 1), n - 2, n / 2}) {
             auto expected = dijkstra.execute(source); 
+            auto realized = bmssp.execute(source);
+            REQUIRE(expected == realized);
+        }
+    }
+}
+
+TEST_CASE("Small random graphs") {
+    using distT = long long;
+    vector<string> paths = {"../tests/graphs/random32D3.gr", "../tests/graphs/random256D3.gr", "../tests/graphs/random1024D3.gr"};
+    for(string path: paths) {
+        auto [adj, m] = readGraph<distT>(path);
+
+        spp::dijkstra<distT> dijkstra(adj);
+        spp::bmssp<distT> bmssp(adj);
+        bmssp.prepare_graph(false);
+
+        int n = adj.size();
+        for(int source: {1, n / 3, n / 2, n / 3 * 2, n - 1}) {
+            auto expected = dijkstra.execute(source);
             auto realized = bmssp.execute(source);
             REQUIRE(expected == realized);
         }
